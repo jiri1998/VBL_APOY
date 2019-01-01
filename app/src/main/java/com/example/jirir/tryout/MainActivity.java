d45604bf1a547b296035dec1eb7a08195666a60e
Jiri Ruysink <jiri1998@gmail.com>
2019-01-01 21:05:38 +0100

    Navigatie functionaeel gemaakt
    navigatie tussen verschillende fragmenten werkt

--- app/src/main/java/com/example/jirir/tryout/MainActivity.java
+++ app/src/main/java/com/example/jirir/tryout/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.jirir.tryout;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -26,20 +29,62 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.navv_navview);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_closed);
 
-                drawer.addDrawerListener(toggle);
-                toggle.syncState();
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.scrlv_fragment_container,
+                    new FragmentHome()).commit();
+        }
+
+        navigationView.setCheckedItem(R.id.item_home);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch (menuItem.getItemId()) {
+            case R.id.item_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.scrlv_fragment_container,
+                        new FragmentHome()).commit();
+
+            case R.id.item_games:
+                getSupportFragmentManager().beginTransaction().replace(R.id.scrlv_fragment_container,
+                        new FragmentGames()).commit();
+
+            case R.id.item_players:
+                getSupportFragmentManager().beginTransaction().replace(R.id.scrlv_fragment_container,
+                        new FragmentPlayers()).commit();
+
+            case R.id.item_standings:
+                getSupportFragmentManager().beginTransaction().replace(R.id.scrlv_fragment_container,
+                        new FragmentStandings()).commit();
+
+            case R.id.item_stats:
+                getSupportFragmentManager().beginTransaction().replace(R.id.scrlv_fragment_container,
+                        new FragmentStats()).commit();
+
+            case R.id.item_settings:
+                getSupportFragmentManager().beginTransaction().replace(R.id.scrlv_fragment_container,
+                        new FragmentSettings()).commit();
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 
     @Override
     public void onBackPressed() {
-        if (drawer.isDrawerOpen(GravityCompat.START)){
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
-        }
-        else {
+        } else {
             super.onBackPressed();
         }
     }
