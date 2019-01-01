a4a8a98861fc6c3a92c93813fed4cc9185492fd2
Jiri Ruysink <jiri1998@gmail.com>
2019-01-01 20:07:32 +0100

    Toevoegingen aan Menu
    - burger Icon met animatio
    - menu over actionbar
    - statusbar transparant bij openen menu

--- app/src/main/java/com/example/jirir/tryout/MainActivity.java
+++ app/src/main/java/com/example/jirir/tryout/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.jirir.tryout;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,11 +15,33 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary("native-lib");
     }
 
+    private DrawerLayout drawer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = findViewById(R.id.tlb_toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_closed);
+
+                drawer.addDrawerListener(toggle);
+                toggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
     }
 
     /**
