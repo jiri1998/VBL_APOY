0148c83b9f6cdbcdf3e57eeba3673ea03fce7d14
Jiri Ruysink <jiri1998@gmail.com>
2019-01-09 12:17:32 +0100

    Communicatie tussen MainActivity en Fragments
    variabelen kunnen worden aangemaakt en meegestuurd naar de fragments vanuit de main activity. Nodig voor versprijden van info uit de database.

--- app/src/main/java/com/example/jirir/tryout/Fragments/FragmentGames.java
+++ app/src/main/java/com/example/jirir/tryout/Fragments/FragmentGames.java
@@ -6,6 +6,7 @@ import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +16,7 @@ import android.widget.Toast;
 
 import com.example.jirir.tryout.Game;
 import com.example.jirir.tryout.GamesListAdapter;
+import com.example.jirir.tryout.MainActivity;
 import com.example.jirir.tryout.R;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -29,59 +31,23 @@ public class FragmentGames extends Fragment {
 
     private static final String TAG = "FragmentGames";
 
-    private static final String Key_date = "date";
-    private static final String Key_name_home = "name_home";
-    private static final String Key_name_away = "name_away";
-    private static final String Key_score_home = "score_home";
-    private static final String Key_score_away = "score_away";
-
-    private FirebaseFirestore db = FirebaseFirestore.getInstance();
-    private DocumentReference dbRef = db.document("Games/Game1");
-
-    String[] dates = {"06/01/2019"};
-    String[] names_home = {"BBC Floorcouture® Zoersel"};
-    String[] names_away = {"BBC Lira Nijlen"};
-    Integer[] scores_home = {70};
-    Integer[] scores_away = {65};
-
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        //items uit database halen
-        dbRef.get().
-                addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                    @Override
-                    public void onSuccess(DocumentSnapshot documentSnapshot) {
-                        if (documentSnapshot.exists()){
-                            dates[0] = documentSnapshot.getString(Key_date);
-                            names_home[0] = documentSnapshot.getString(Key_name_home);
-                            names_away[0] = documentSnapshot.getString(Key_name_away);
-                            scores_home[0] = documentSnapshot.getDouble(Key_score_home).intValue();
-                            scores_away[0] = documentSnapshot.getDouble(Key_score_away).intValue();
-                        }
-                        else{
-                            Toast.makeText(FragmentGames.this.getContext(), "failed to load games from database", Toast.LENGTH_LONG).show();
-                        }
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-
-                    }
-                });
 
         //items in list zetten
         View view = inflater.inflate(R.layout.fragment_games, container, false);
 
-        Game one = new Game(dates[0],names_home[0], names_away[0],scores_home[0], scores_away[0]);
-        Game two = new Game("05/01/2019", "Antwerp Giants", "BBC Floorcouture® Zoersel", 64, 45);
+        Bundle b = getArguments();
+        String result = b.getString("tmname");
+        Log.d(TAG, b.getString("tmname"));
+
+        Game two = new Game("05/01/2019", result, "BBC Floorcouture® Zoersel", 64, 45);
         Game three = new Game("06/01/2019", "Antwerp Giants", "BBC Floorcouture® Zoersel", 99, 76);
         Game four = new Game("08/01/2019", "Antwerp Giants", "BBC Floorcouture® Zoersel", 100, 102);
         Game five = new Game("12/01/2019", "Antwerp Giants", "BBC Floorcouture® Zoersel", 101, 88);
 
         ArrayList<Game> gamesList = new ArrayList<>();
-        gamesList.add(one);
         gamesList.add(two);
         gamesList.add(three);
         gamesList.add(four);
--- app/src/main/java/com/example/jirir/tryout/MainActivity.java
+++ app/src/main/java/com/example/jirir/tryout/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.jirir.tryout;
 
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -8,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.jirir.tryout.Fragments.FragmentGames;
 import com.example.jirir.tryout.Fragments.FragmentHome;
@@ -15,13 +21,28 @@ import com.example.jirir.tryout.Fragments.FragmentPlayers;
 import com.example.jirir.tryout.Fragments.FragmentSettings;
 import com.example.jirir.tryout.Fragments.FragmentStandings;
 import com.example.jirir.tryout.Fragments.FragmentStats;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
+    private static final String Key_date = "date";
+    private static final String Key_name_home = "name_home";
+    private static final String Key_name_away = "name_away";
+    private static final String Key_score_home = "score_home";
+    private static final String Key_score_away = "score_away";
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private DocumentReference dbRef = db.document("Games/Game1");
+
+    private final String[] dates = {"06/01/2019"};
+    String[] names_home = {"BBC Floorcouture® Zoersel"};
+    String[] names_away = {"BBC Lira Nijlen"};
+    Integer[] scores_home = {70};
+    Integer[] scores_away = {65};
 
     private DrawerLayout drawer;
 
@@ -60,28 +81,74 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     }
 
+    //
+
+    public void database(View view){
+
+        dbRef.get().
+                addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        if (documentSnapshot.exists()){
+                            dates[0] = documentSnapshot.getString(Key_date);
+                            names_home[0] = documentSnapshot.getString(Key_name_home);
+                            names_away[0] = documentSnapshot.getString(Key_name_away);
+                            scores_home[0] = documentSnapshot.getDouble(Key_score_home).intValue();
+                            scores_away[0] = documentSnapshot.getDouble(Key_score_away).intValue();
+                        }
+                        else{
+                            Toast.makeText(MainActivity.this, "failed to load games from database", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+    }
+
     // Menu code
 
     @Override
     public boolean onNavigationItemSelected(MenuItem menuItem) {
 
+        FragmentManager manager = getSupportFragmentManager();
+        android.support.v4.app.FragmentTransaction transaction = manager.beginTransaction();
+
+        final FragmentHome fragmentHome = new FragmentHome();
+        final FragmentGames fragmentGames = new FragmentGames();
+        final FragmentPlayers fragmentPlayers = new FragmentPlayers();
+        final FragmentStandings fragmentStandings = new FragmentStandings();
+
         switch (menuItem.getItemId()) {
             case R.id.item_home:
-                getSupportFragmentManager().beginTransaction().replace(R.id.scrlv_fragment_container,
-                        new FragmentHome()).commit();
+                Bundle bundlefh = new Bundle();
+                //b.putString("tmname", "Hopelijk Werkt dit");
+                fragmentHome.setArguments(bundlefh);
+                transaction.replace(R.id.scrlv_fragment_container, fragmentHome).commit();
                 break;
 
             case R.id.item_games:
-                getSupportFragmentManager().beginTransaction().replace(R.id.scrlv_fragment_container,
-                        new FragmentGames()).commit();
+                Bundle bundlefg = new Bundle();
+                //bundlefg.putString("tmname", "Hopelijk Werkt dit");
+                fragmentGames.setArguments(bundlefg);
+                transaction.replace(R.id.scrlv_fragment_container,fragmentGames).commit();
                 break;
 
             case R.id.item_players:
+                Bundle bundlefp = new Bundle();
+                //bundlefg.putString("tmname", "Hopelijk Werkt dit");
+                fragmentGames.setArguments(bundlefp);
                 getSupportFragmentManager().beginTransaction().replace(R.id.scrlv_fragment_container,
                         new FragmentPlayers()).commit();
                 break;
 
             case R.id.item_standings:
+                Bundle bundlefs = new Bundle();
+                //bundlefg.putString("tmname", "Hopelijk Werkt dit");
+                fragmentGames.setArguments(bundlefs);
                 getSupportFragmentManager().beginTransaction().replace(R.id.scrlv_fragment_container,
                         new FragmentStandings()).commit();
                 break;
@@ -109,10 +176,4 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             super.onBackPressed();
         }
     }
-
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
 }
